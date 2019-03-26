bd879adcb43b282b22a7c4341d5cdf84813106aa
jawyuhz <rtsui9797@gmail.com>
2019-03-25 23:04:18 -0400

    Basic login page completed

--- duct-drone-app/src/App.js
+++ duct-drone-app/src/App.js
@@ -3,30 +3,32 @@ import Navigation from './Navigation';
 import DataTable from './DataTable';
 import Container from 'react-bootstrap/Container';
 import Control from './Control';
+import Login from './Login';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
 import './App.css';
 class App extends Component {
-
 	render() {
-		return(
-			<Router>
-				<Container>
-					<Row>
-						<Col><div id="feed"></div></Col>
-					</Row>
-			        <Row>
-			          <Col>
-			          	<Route path="/" exact component={Control}/>
-						<Route path="/data/" component={DataTable}/>
-					  </Col>
-			        </Row>
-<Navigation/>
-				</Container>
-			</Router>
-		);
+		return (<Login/>);
+		
+		// return(
+		// 	<Router>
+		// 		<Container>
+		// 			<Row>
+		// 				<Col><div id="feed"></div></Col>
+		// 			</Row>
+		// 	        <Row>
+		// 	          <Col>
+		// 	          	<Route path="/" exact component={Control}/>
+		// 				<Route path="/data/" component={DataTable}/>
+		// 			  </Col>
+		// 	        </Row>
+		// 			<Navigation/>
+		// 		</Container>
+		// 	</Router>
+		// );
 	}
 }
 export default App;
--- duct-drone-app/src/Navigation.js
+++ duct-drone-app/src/Navigation.js
@@ -8,21 +8,20 @@ class Navigation extends React.Component {
   }
   render() {
     return (
-
-              <Nav fill variant="pills" defaultActiveKey="1" onSelect={k => this.handleSelect(k)} fixed='bottom'>
-                <Nav.Item>
-                    <Link to="/">Control</Link>
-                </Nav.Item>
-                <Nav.Item>
-                    <Link to="/data/">Data</Link>
-                </Nav.Item> 
-                <Nav.Item>
-                  Logs
-                </Nav.Item>
-                <Nav.Item>
-                  View Map
-                </Nav.Item>
-              </Nav>
+      <Nav fill variant="pills" defaultActiveKey="1" onSelect={k => this.handleSelect(k)} fixed='bottom'>
+        <Nav.Item>
+            <Link to="/">Control</Link>
+        </Nav.Item>
+        <Nav.Item>
+            <Link to="/data/">Data</Link>
+        </Nav.Item> 
+        <Nav.Item>
+          Logs
+        </Nav.Item>
+        <Nav.Item>
+          View Map
+        </Nav.Item>
+      </Nav>
     )
   }
 }
