94c1d7484d371b3ff1a0d168d4449c2a7308fff9
Jawyuhz <rtsui9797@gmail.com>
2019-04-07 14:00:46 -0400

    Splitted models into dedicated files

---
+++ duct-drone-app/server/models/floormodel.js
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose';
+const Schema = mongoose.Schema;
+
+const floorSchema = new Schema({
+    name: String,
+    map_link: String,
+    date: Date,
+    sensorData: [{ type: Schema.Types.ObjectId, ref: 'Entry' }]
+});
+
+module.exports = mongoose.model('Floor', floorSchema);
