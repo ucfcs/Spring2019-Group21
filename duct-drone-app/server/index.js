94517633f9bc2d5b35ae3b98b7ec481e1ac9204e
jawyuhz <rtsui9797@gmail.com>
2019-04-09 20:21:32 -0400

    etnrymodel id removed

--- duct-drone-app/server/index.js
+++ duct-drone-app/server/index.js
@@ -25,13 +25,18 @@ var mongoDB = `mongodb://${server}/${database}`;
 mongoose.connect(mongoDB, { useNewUrlParser: true });
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+
 var testdata = new Entry({
 	coordinate:[{ x:32, y:32, z:23}],
 	time: new Date(),
 	temperature: 32.3,
 	air_velocity: 35.2,
 });
-
+testdata.save(function (err) {
+  if (err) return handleError(err);
+  console.log("SAVED");
+});
 app.get("/adddata", (req, res) => {
 	var testdata = new Entry({
 	coordinate:[{ x:32, y:32, z:23}],
--- duct-drone-app/server/models/entrymodel.js
+++ duct-drone-app/server/models/entrymodel.js
@@ -2,7 +2,6 @@ var mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const entrySchema = new Schema({
-    _id: Schema.Types.ObjectId,
     coordinate: [{ x: Number, y: Number, z: Number }],
     time: Date,
     temperature: Schema.Types.Decimal128,
