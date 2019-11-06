08bcae35abd70d57d238aaf2bc3fde1d29203888
jawyuhz <rtsui9797@gmail.com>
2019-11-06 01:37:42 -0500

    fixing hook cycle bug

--- duct-drone-app/src/App.jsx
+++ duct-drone-app/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Container from 'react-bootstrap/Container';
 import {
-  Button, ButtonToolbar, Row, Col, Form
+  Button, ButtonToolbar, Row, Col, Form,
 } from 'react-bootstrap';
 import {
   BrowserRouter as Router,
@@ -13,7 +13,7 @@ import ManageModal from './components/ManageModal';
 import SessionTable from './components/SessionTable';
 import './components/styles/App.css';
 
-var ROSLIB = require('roslib');
+const ROSLIB = require('roslib');
 
 class App extends Component {
   constructor(props) {
@@ -40,21 +40,20 @@ class App extends Component {
       rightPressed: false,
       leftPressed: false,
       upPressed: false,
-      downPressed:false,
+      downPressed: false,
       linear: {
-        x : 0.0,
-        y : 0.0,
-        z : 0.0
-      }
+        x: 0.0,
+        y: 0.0,
+        z: 0.0,
+      },
     };
   }
 
   componentDidMount() {
     // if(this.state.rosConnected) {
-      document.addEventListener('keydown', this.keyDownHandler, false);
-      document.addEventListener('keyup', this.keyUpHandler, false);
+    document.addEventListener('keydown', this.keyDownHandler, false);
+    document.addEventListener('keyup', this.keyUpHandler, false);
     // }
-    
   }
 
   componentWillUnmount() {
@@ -63,8 +62,8 @@ class App extends Component {
   }
 
   getData = () => {
-    console.log("get");
-    fetch('http://'+this.state.serverIP + '/api/get/maps',
+    console.log('get');
+    fetch(`http://${this.state.serverIP}/api/get/maps`,
       {
         method: 'GET',
       })
@@ -73,122 +72,119 @@ class App extends Component {
         this.setState({ logData: data });
       });
   }
+
   updateServerIP = (event) => {
     console.log(event.target.value);
-    this.setState({ serverIP: event.target.value});
+    this.setState({ serverIP: event.target.value });
   }
+
   updateROSIP = (event) => {
-    this.setState({ ROSIP: event.target.value});
+    this.setState({ ROSIP: event.target.value });
   }
+
   updateSessionName = (name) => {
-    this.setState({ sessionName: name});
+    this.setState({ sessionName: name });
   }
+
   updateSessionID = (id) => {
     this.setState({ sessionID: id });
   }
+
   connectServer = () => {
-    let regex = "^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]):[0-9]+$";
-    this.setState({ serverConnected: false});
-    this.setState({ logData: [] })
-      fetch('http://' + this.state.serverIP + '/api/get/maps',
+    const regex = '^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]):[0-9]+$';
+    this.setState({ serverConnected: false });
+    this.setState({ logData: [] });
+    fetch(`http://${this.state.serverIP}/api/get/maps`,
       {
         method: 'GET',
       })
-      .then(response => 
-        {
-          if(response.ok){
-            this.setState({ serverConnected: true });
-            this.getData();
-          }
-        })
+      .then((response) => {
+        if (response.ok) {
+          this.setState({ serverConnected: true });
+          this.getData();
+        }
+      });
   }
+
   connectROS = () => {
     // let regex = "^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]):[0-9]+$";
     // if(!this.state.ROSIP.match(regex))
     //   console.log("please enter a valid ROS IP");
     // else {
-      let rosSession = new ROSLIB.Ros
-      ({
-        url : 'ws://' + this.state.ROSIP + ":9090"
-      });
-      this.setState({ ros: rosSession});
-        var IRListener = new ROSLIB.Topic({
-          ros: rosSession,
-          name: '/ir_data',
-          messageType: 'std_msgs/Float32MultiArray'
-        });
-        var HumidityListener = new ROSLIB.Topic({
-          ros: rosSession,
-          name: '/humidity_data',
-          messageType: 'std_msgs/Float32MultiArray'
-        });
-        IRListener.subscribe( (message) => console.log(message));
-        HumidityListener.subscribe( (message) =>  {
-          console.log(message);
-          this.setState({ currentTemp: Math.round( message.data[0] * 10) / 10});
-          this.setState({ currentHumidity: Math.round( message.data[1] * 10) / 10});
-          const { sessionID } = this.state;
-          const data = {
-              temperature: this.state.currentTemp,
-              humidity: this.state.currentHumidity
-          };
-          console.log("REACH FETCHING");
-          fetch('http://'+this.state.serverIP + '/api/update/map/' + sessionID,
-          {
-            method: 'PUT',
-            headers: {
-              'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(data),
-          }).then( () => 
-          console.log("updated sensor record"))
-          
-        });
-
-
+    const rosSession = new ROSLIB.Ros({
+      url: `ws://${this.state.ROSIP}:9090`,
+    });
+    this.setState({ ros: rosSession });
+    const IRListener = new ROSLIB.Topic({
+      ros: rosSession,
+      name: '/ir_data',
+      messageType: 'std_msgs/Float32MultiArray',
+    });
+    const HumidityListener = new ROSLIB.Topic({
+      ros: rosSession,
+      name: '/humidity_data',
+      messageType: 'std_msgs/Float32MultiArray',
+    });
+    IRListener.subscribe(message => console.log(message));
+    HumidityListener.subscribe((message) => {
+      console.log(message);
+      this.setState({ currentTemp: Math.round(message.data[0] * 10) / 10 });
+      this.setState({ currentHumidity: Math.round(message.data[1] * 10) / 10 });
+      const { sessionID } = this.state;
+      const data = {
+        temperature: this.state.currentTemp,
+        humidity: this.state.currentHumidity,
+      };
+      console.log('REACH FETCHING');
+      fetch(`http://${this.state.serverIP}/api/update/map/${sessionID}`,
+        {
+          method: 'PUT',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(data),
+        }).then(() => console.log('updated sensor record'));
+    });
   }
+
   keyDownHandler = (event) => {
-    if(!this.state.keyFired) {
-      this.setState({ keyFired: true})
-    
-    console.log("key down" + event.key);
-    if(event.keyCode == 68) {
-        this.setState({rightPressed: true})
-        this.move (   0, -0.5);
-    }
-    else if(event.keyCode == 65) {
-      this.setState({leftPressed: true})
-      this.move (   0, 0.5);
-    }
-    if(event.keyCode == 83) {
-      this.setState({downPressed: true})
-      this.move (-0.15, 0);
-    }
-    else if(event.keyCode == 87) {
-      this.setState({upPressed: true})
-      this.move ( 0.15,   0);
+    if (!this.state.keyFired) {
+      this.setState({ keyFired: true });
+
+      console.log(`key down${event.key}`);
+      if (event.keyCode == 68) {
+        this.setState({ rightPressed: true });
+        this.move(0, -0.5);
+      } else if (event.keyCode == 65) {
+        this.setState({ leftPressed: true });
+        this.move(0, 0.5);
+      }
+      if (event.keyCode == 83) {
+        this.setState({ downPressed: true });
+        this.move(-0.15, 0);
+      } else if (event.keyCode == 87) {
+        this.setState({ upPressed: true });
+        this.move(0.15, 0);
+      }
     }
   }
-}
 
 keyUpHandler = (event) => {
-    this.setState({ keyFired: false})
-    console.log("key up" + event.key);
-    if(event.keyCode == 68) {
-      this.setState({rightPressed: false})
-      this.move (   0,0);
+  this.setState({ keyFired: false });
+  console.log(`key up${event.key}`);
+  if (event.keyCode == 68) {
+    this.setState({ rightPressed: false });
+    this.move(0, 0);
+  } else if (event.keyCode == 65) {
+    this.setState({ leftPressed: false });
+    this.move(0, 0);
   }
-  else if(event.keyCode == 65) {
-    this.setState({leftPressed: false})
-    this.move (   0,0);
-  }
-  if(event.keyCode == 83) {
-    this.setState({downPressed: false})
-    this.move (   0,0);
-  }
-  else if(event.keyCode == 87) {
-    this.setState({upPressed: false})
-    this.move (   0,0);
+  if (event.keyCode == 83) {
+    this.setState({ downPressed: false });
+    this.move(0, 0);
+  } else if (event.keyCode == 87) {
+    this.setState({ upPressed: false });
+    this.move(0, 0);
   }
 }
 
@@ -199,58 +195,59 @@ keyUpHandler = (event) => {
   openSessionModal = () => {
     this.setState({ sessionModalOpen: true });
   }
+
   endSession = () => {
     this.setState({ sessionName: '' });
     this.setState({ sessionID: '' });
   }
+
   closeModal = () => {
     this.setState({ manageModalOpen: false });
-    this.setState({ sessionModalOpen: false});
+    this.setState({ sessionModalOpen: false });
   }
 
 
   startListen = () => {
-    console.log("break");
-    
-    this.state.listener.subscribe(function(message) {
+    console.log('break');
+
+    this.state.listener.subscribe((message) => {
       console.log(message);
     });
   }
+
   stopList = () => {
     this.state.listener.unsubscribe();
   }
 
-  move = (linearx, rotatez) =>
-  {
-    console.log("tool");
+  move = (linearx, rotatez) => {
+    console.log('tool');
     // Create the velocity command
-    let cmdVel = new ROSLIB.Topic
-    ({
-      ros : this.state.ros,
-      name : '/cmd_vel',
-      messageType : 'geometry_msgs/Twist'
+    const cmdVel = new ROSLIB.Topic({
+      ros: this.state.ros,
+      name: '/cmd_vel',
+      messageType: 'geometry_msgs/Twist',
     });
 
     // Create the twist message
-    var twist = new ROSLIB.Message
-    ({
-      linear : 
+    const twist = new ROSLIB.Message({
+      linear:
       {
-        x : linearx,
-        y : 0.0,
-        z : 0.0
+        x: linearx,
+        y: 0.0,
+        z: 0.0,
       },
-      angular : 
+      angular:
       {
-        x : 0.0,
-        y : 0.0,
-        z : rotatez
-      }
+        x: 0.0,
+        y: 0.0,
+        z: rotatez,
+      },
     });
     console.log(twist);
     // Publishing the twist message
     cmdVel.publish(twist);
   }
+
   render() {
     Number.prototype.pad = function (size) {
       let s = String(this);
@@ -271,8 +268,9 @@ keyUpHandler = (event) => {
               closeModal={this.closeModal}
               updateSessionName={this.updateSessionName}
               updateSessionID={this.updateSessionID}
-              sessionID={sessionID} 
-              serverIP={serverIP}/>
+              sessionID={sessionID}
+              serverIP={serverIP}
+            />
             <Row style={{ height: '100%' }}>
               <Col>
                 <Sidebar
@@ -285,7 +283,8 @@ keyUpHandler = (event) => {
                   connectServer={this.connectServer}
                   connectROS={this.connectROS}
                   updateROSIP={this.updateROSIP}
-                  updateServerIP={this.updateServerIP} />
+                  updateServerIP={this.updateServerIP}
+                />
               </Col>
               <Col xs={8}>
                 <Container fluid={true}>
@@ -301,31 +300,35 @@ keyUpHandler = (event) => {
                       <Col>
                         <Button
                           variant={sessionID ? 'success' : 'warning'}
-                          className="btn-display" disabled={true}
-                          size="lg">
-                          {sessionID !='' ? sessionID : 'Inactive'}
+                          className="btn-display"
+                          disabled={true}
+                          size="lg"
+                        >
+                          {sessionID != '' ? sessionID : 'Inactive'}
                         </Button>
                       </Col>
                       <Col>
                         <Button
                           variant={sessionName != '' ? 'success' : 'warning'}
-                          className="btn-display" disabled={true}
-                          size="lg">
+                          className="btn-display"
+                          disabled={true}
+                          size="lg"
+                        >
                           {sessionName != '' ? sessionName : 'Inactive'}
                         </Button>
                       </Col>
-                      
+
                     </Row>
                   </ButtonToolbar>
 
                 </Container>
                 <Container fluid={true}>
-                  <img src={"http://" + this.state.ROSIP + ":8080" + "/stream?topic=/raspicam_node/image_raw&quality=100&invert=true"}/>
+                  <img src={`http://${this.state.ROSIP}:8080` + '/stream?topic=/raspicam_node/image_raw&quality=100&invert=true'} />
                   <SessionTable data={logData.length ? logData[0].sensorData : []} />
                 </Container>
               </Col>
               <Col>
-                <LiveBar currentTemp={currentTemp} currentHumidity={currentHumidity}/>
+                <LiveBar currentTemp={currentTemp} currentHumidity={currentHumidity} />
               </Col>
             </Row>
           </Container>
