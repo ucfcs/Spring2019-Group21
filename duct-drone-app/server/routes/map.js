0de3317f6784d687f657c1f18981b3685b61b69c
Jawyuhz <rtsui9797@gmail.com>
2019-11-29 12:49:51 -0500

    changed add sensordata route from /update/map/:id to /update/map/:id/SensorData

--- duct-drone-app/server/routes/map.js
+++ duct-drone-app/server/routes/map.js
@@ -4,7 +4,7 @@ module.exports = function (router) {
   router.get('/get/maps', Map.getMaps);
   router.post('/create/map', Map.createMap);
   router.post('/create/maps', Map.createMaps);
-  router.put('/update/map/:id', Map.addSensorData);
+  router.put('/update/map/:id/SensorData', Map.addSensorData);
   router.delete('/remove/:id', Map.removeMap);
   router.delete('/removeall', Map.removeMaps);
 };
